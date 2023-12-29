c826c33a4a7567da41bb959140f508ea15194855
codenameCookie <55008091+CodenameCookie@users.noreply.github.com>
2023-12-29 05:21:59 +0000

    Fix missing file output section and add txt file example

--- powershell/tree-exclude_modules.ps1
+++ powershell/tree-exclude_modules.ps1
@@ -1,4 +1,4 @@
-tree-exclude_modules.ps1$nodeModulesFound = $false
+$nodeModulesFound = $false
 Tree /F /A | ForEach-Object {
     if ($_ -match '^\|   \+---node_modules$') {
         # When node_modules is found, set flag to true and output the line
@@ -9,3 +9,9 @@ Tree /F /A | ForEach-Object {
         # If another directory at the same level as node_modules is found, reset the flag
         $nodeModulesFound = $false
     }
+
+    # Output the line if it's not under node_modules
+    if (-not $nodeModulesFound) {
+        $_
+    }
+} > ProjectTreeStructure.txt
